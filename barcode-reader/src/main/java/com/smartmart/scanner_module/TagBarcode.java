d929b5696d6e3c00e096cfc46a5c9054d6aee3df
HARPREET SINGH BRAR <51008350+brarharry@users.noreply.github.com>
2019-10-23 22:37:30 -0700

    Initial commit

---
+++ barcode-reader/src/main/java/com/smartmart/scanner_module/TagBarcode.java
@@ -0,0 +1,28 @@
+package com.smartmart.scanner_module;
+
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.smartmart.scanner_module.camera.Overlay;
+
+/**
+ * Factory for creating a tracker and associated graphic to be associated with a new barcode.  The
+ * multi-processor uses this factory to create barcode trackers as needed -- one for each barcode.
+ */
+class TagBarcode implements MultiProcessor.Factory<Barcode> {
+    private Overlay<ColorSetting> mOverlay;
+    private ScannerActivity.BarcodeGraphicTrackerListener listener;
+
+    TagBarcode(Overlay<ColorSetting> barcodeOverlay, ScannerActivity.BarcodeGraphicTrackerListener listener) {
+        mOverlay = barcodeOverlay;
+        this.listener = listener;
+    }
+
+    @Override
+    public Tracker<Barcode> create(Barcode barcode) {
+        ColorSetting graphic = new ColorSetting(mOverlay);
+        return new ScannerActivity(mOverlay, graphic, listener);
+    }
+
+}
+
