76b8e9113717197c372062d4bdc3c725014b80a5
HARPREET SINGH BRAR <51008350+Harpreet-Brar@users.noreply.github.com>
2019-12-05 10:33:53 -0800

    Create google-services.js

---
+++ app/google-services.js
@@ -0,0 +1,48 @@
+{
+  "project_info": {
+    "project_number": "516365491777",
+    "firebase_url": "https://capstone-287b2.firebaseio.com",
+    "project_id": "capstone-287b2",
+    "storage_bucket": "capstone-287b2.appspot.com"
+  },
+  "client": [
+    {
+      "client_info": {
+        "mobilesdk_app_id": "1:516365491777:android:688d8c7a0a4ea64056f0c0",
+        "android_client_info": {
+          "package_name": "com.smartmart.scanner"
+        }
+      },
+      "oauth_client": [
+        {
+          "client_id": "516365491777-li229fsdammcpv6sqp6uvdn1n97d6oe7.apps.googleusercontent.com",
+          "client_type": 1,
+          "android_info": {
+            "package_name": "com.smartmart.scanner",
+            "certificate_hash": "19d0d74a5ae647bd5ccde3783cf4f365dd8103af"
+          }
+        },
+        {
+          "client_id": "516365491777-uk089jelg4ghqcad5gtgtp6nso6ap0sd.apps.googleusercontent.com",
+          "client_type": 3
+        }
+      ],
+      "api_key": [
+        {
+          "current_key": "AIzaSyAOLn9aDM0CPxEu3_BSiwI40xkSo2epixg"
+        }
+      ],
+      "services": {
+        "appinvite_service": {
+          "other_platform_oauth_client": [
+            {
+              "client_id": "516365491777-uk089jelg4ghqcad5gtgtp6nso6ap0sd.apps.googleusercontent.com",
+              "client_type": 3
+            }
+          ]
+        }
+      }
+    }
+  ],
+  "configuration_version": "1"
+}
